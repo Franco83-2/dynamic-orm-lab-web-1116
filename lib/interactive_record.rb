a05f11b8479cfadccc10bb9292e19374ba9d970b
franco83-2 <franco.84@gmail.com>
2016-12-12 12:06:03 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,7 +1,4 @@
 require_relative "../config/environment.rb"
-require 'active_support/inflector'
-require 'pry'
-
 class InteractiveRecord
 
   def self.table_name
@@ -17,8 +14,7 @@ class InteractiveRecord
   end
 
   def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
@@ -35,14 +31,11 @@ class InteractiveRecord
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-    DB[:conn].execute(sql)
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = '#{name}'")
   end
 
-
-    def self.find_by(something)
-      sql = "SELECT * FROM #{self.table_name} WHERE #{something.keys[0]} LIKE '%#{something.values[0]}%'"
-      DB[:conn].execute(sql)
-    end
+  def self.find_by(something)
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{something.keys[0]} LIKE '%#{something.values[0]}%'")
+  end
 
 end
